de0356b2e0379c160c7479951a6cce6af37d80c9
jpdyuki <joypad.y.t.h@gmail.com>
2017-08-23 00:12:03 +0800

    improve user experience

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,7 +2,33 @@ function collapse_menu(){
     $(".navbar-collapse").collapse('hide');
 }
 
+function update_boundary(){
+    _scrollTop_Max = $(document).height() - $(window).height() - 100;
+    toggle_btn_top();
+}
+
+function toggle_btn_top(){
+    var x = $(window).scrollTop();
+    if( (x > _scrollTop_Max) || (x > ($(window).height() << 1)) )
+        $('#btn-top').removeClass('loading');
+    else
+        $('#btn-top').addClass('loading');
+}
+
+function back_to_top(){
+    $('html, body').animate({
+        scrollTop: 0
+    }, 700, function(){
+        window.location.hash = '#';
+    });
+}
+
 $(function(){
     // collapse menu on click
     $(window).click(collapse_menu).scroll(collapse_menu);
+    // back to top button
+    $(window).resize(update_boundary);
+    $(window).scroll(toggle_btn_top);
+    update_boundary();
+    $('#btn-top').click(back_to_top);
 });
