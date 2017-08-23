a7126a8e487a9bf464d70d46c362670b39b822d3
jpdyuki <joypad.y.t.h@gmail.com>
2017-08-24 01:50:34 +0800

    add a search panel on selection

--- scripts/article.js
+++ scripts/article.js
@@ -42,11 +42,38 @@ function toggle_furigana() {
     }
 }
 
+function track_mouse_pos(e){
+    _mouse_x = e.pageX;
+    _mouse_y = e.pageY;
+}
+
+function show_ext_link(){
+    $('rt, .cp-ignore').css('visibility', 'hidden');
+    var sel = window.getSelection().toString();
+    $('rt, .cp-ignore').css('visibility', 'visible');
+    $('#btn-ext-end').prop('href', "http://end-proj.herokuapp.com/?q=" + sel);
+    $('#ojad-text').val(sel);
+    if(sel === "")
+        hide_ext_link();
+    else
+        $('#ext-link').css({ top: _mouse_y+20+'px', left: (_mouse_x-60)+'px'}).removeClass('hidden');
+}
+
+function hide_ext_link(){
+    $('#ext-link').addClass('hidden');
+}
+
 $(function(){
+    // auto scroll to hash
     if(location.hash) scr_shift();
     $('div[id^="L"]>a').click(scr_to_hash_clk());
     $(window).on('hashchange', scr_to_hash_ld());
+    // utilities on selection
+    $(window).on('mousemove', track_mouse_pos);
+    $(document).on('selectionchange', show_ext_link);
+    // hint on pager
     $('[data-toggle="tooltip"]').tooltip();
+    // load ruby config
     $('#ruby-switch').on('change', toggle_furigana);
     if(!Cookies.get('noruby'))
         $('#ruby-switch').prop("checked", true).trigger('change');
